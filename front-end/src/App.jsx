d3dc8f81decaf5c280b63bb5f8dfa4dcd75293fb
Darlon Gomes <gomesdarlon@icloud.com>
2022-09-22 23:21:53 -0300

    chore: initial setup

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  return(
+      <>
+            <CssBaseline />
+            Olá
+      </>
+  );
+}
+
+export default App;
---
+++ front-end/src/pages/WelcomePage/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Ambient = styled.div`
+  width: 100vw;
+`;
+
+export { Ambient};
